0676a5ba2be8701aa8706810808df4c6505d7a94
Nicholas Robinson <nick_rocks_24_7@hotmail.com>
2017-08-29 20:27:14 -0400

    trying to fix

--- config.js
+++ config.js
@@ -1,20 +1,23 @@
 app.config(function($routeProvider, $locationProvider) {
+
+    // HTML5 History API
+    $locationProvider.html5Mode(true);
+
     $routeProvider
     .when("/", {
         templateUrl: "partials/home.html"
     })
     .when("/projects", {
         templateUrl: "partials/projects.html",
-        controller: "projectsCtrl as ctrl"
+        controller: 'projectsCtrl'
     })
     .when("/experience", {
         templateUrl: "partials/experience.html",
-        controller: experienceCtrl
+        controller: "experienceCtrl as ctrl"
     })
     .otherwise({
         redirectTo: "/"
     });
 
-    // HTML5 History API
-    // $locationProvider.html5Mode(true);
+    
 });
