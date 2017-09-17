a9933164fd670e88a953d0f1cbc938be7ced28bd
LAPTOP-RGUBM3PA\Nick <nick_rocks_24_7@hotmail.com>
2017-09-16 23:09:38 -0400

    icon change

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -2,17 +2,17 @@ app.controller('homeCtrl', function($scope) {
     $scope.projects = [
         {   image: "assets/images/rogue-arrow1.png",
             name: "Rogue Arrow",
-            by: "Me",
+            by: "Me, Ron Gayowsky, Jeffrey Langballe",
             description: "Run, jump, and shoot arrows to defeat enemies in my highschool group's 2D C# project.",
             github: "https://github.com/nrobinson12/Rogue-Arrow"
         },
-        {   image: "assets/images/dueality.png",
+        {   image: "assets/images/duealitystanding.png",
             name: "Dueality",
-            by: "Nicholas Robinson, Daniel Molka, Anirudh Iyer, Divit Sharma",
+            by: "Me, Daniel Molka, Anirudh Iyer, Divit Sharma",
             description: "During the 2017 Winter Game Jam, our team won the \"Most Wanted to See Finished\" award for our 2D platformer.",
             github: "https://github.com/nrobinson12/Deuality"
         },
-        {   image: "assets/images/pong.png",
+        {   image: "assets/images/cpong.png",
             name: "Classic Pong",
             by: "Me",
             description: "My take on the classic game, using C# in Visual Studio.",
