b6b9a0afd5b5912bebb9aa46a40e6cd0f74c17da
Nicholas Robinson <nick_rocks_24_7@hotmail.com>
2017-10-27 16:13:43 -0400

    fixing up the projects

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -1,28 +1,9 @@
 app.controller('homeCtrl', function($scope) {
     $scope.projects = [
-        {   image: "assets/images/rogue-arrow1.png",
-            name: "Rogue Arrow",
-            by: "Me, Ron Gayowsky, Jeffrey Langballe",
-            description: "Run, jump, and shoot arrows to defeat enemies in my highschool group's 2D C# project.",
-            github: "https://github.com/nrobinson12/Rogue-Arrow"
-        },
-        {   image: "assets/images/duealitystanding.png",
-            name: "Dueality",
-            by: "Me, Daniel Molka, Anirudh Iyer, Divit Sharma",
-            description: "During the 2017 Winter Game Jam, our team won the \"Most Wanted to See Finished\" award for our 2D platformer.",
-            github: "https://github.com/nrobinson12/Deuality"
-        },
-        {   image: "assets/images/cpong.png",
-            name: "Classic Pong",
-            by: "Me",
-            description: "My take on the classic game, using C# in Visual Studio.",
-            github: "https://github.com/nrobinson12/Classic-Pong"
-        }
-    ];
-    $scope.jobs = [
-        {   image: "assets/images/viziya.png",
-            company: "VIZIYA",
+        {   image: "assets/images/viziya2.png",
+            name: "Viziya",
             date: "May 1 2017 - present",
+            what: "Work Experience",
             sections: [
                 {   title: "Role",
                     description: "Junior Software Developer"
@@ -43,6 +24,27 @@ app.controller('homeCtrl', function($scope) {
                     description: "This is an amazing company to work with. I feel right at home with the open and very friendly employees, providing a unique and lasting co-op experience. They are always willing to talk and help each other whenever is needed, making sure everyone was working as efficiently as possible. I am absolutely enjoying my time here, and I recommend this company to anyone looking for a great co-op placement."
                 }
             ]
+        },
+        {   image: "assets/images/rogue-arrow.png",
+            name: "Rogue Arrow",
+            by: "Me, Ron Gayowsky, Jeffrey Langballe",
+            description: "Run, jump, and shoot arrows to defeat enemies in my highschool group's 2D C# project.",
+            github: "https://github.com/nrobinson12/Rogue-Arrow",
+            what: "Game"
+        },
+        {   image: "assets/images/dueality.png",
+            name: "Dueality",
+            by: "Me, Daniel Molka, Anirudh Iyer, Divit Sharma",
+            description: "During the 2017 Winter Game Jam, our team won the \"Most Wanted to See Finished\" award for our 2D platformer.",
+            github: "https://github.com/nrobinson12/Deuality",
+            what: "Game"
+        },
+        {   image: "assets/images/pong.png",
+            name: "Classic Pong",
+            by: "Me",
+            description: "My take on the classic game, using C# in Visual Studio.",
+            github: "https://github.com/nrobinson12/Classic-Pong",
+            what: "Game"
         }
     ];
 });
